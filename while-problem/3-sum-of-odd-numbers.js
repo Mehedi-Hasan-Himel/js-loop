25aca0b71cdff2c7f524b4470f1b3df43dc24268
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-04 10:43:28 +0600

    oddSum , evenSum

---
+++ while-problem/3-sum-of-odd-numbers.js
@@ -0,0 +1,40 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+*/
+
+let numbers = 81;
+let sum = 0;
+while (numbers <= 131) {
+  if (numbers % 2 !== 0) {
+    sum = sum + numbers;
+  }
+  numbers++;
+}
+console.log(`Display sum of all the odd numbers from 81 to 131: `, sum);
+
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+let num = 206;
+let evenSum = 0;
+while (num <= 311) {
+  if (num % 2 == 0) {
+    evenSum = evenSum + num;
+  }
+  num++;
+}
+
+console.log(
+  `Display sum of all the even numbers from 206 to 311.
+: `,
+  evenSum
+);
---
+++ while-problem/tempCodeRunnerFile.js
@@ -0,0 +1,27 @@
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+*/
+
+let numbers = 81;
+let sum = 0;
+while (numbers <= 131) {
+  if (numbers % 2 !== 0) {
+    sum = sum + numbers;
+  }
+  numbers++;
+}
+console.log(`Display sum of all the odd numbers from 81 to 131.`, sum);
+
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
