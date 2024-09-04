836b18e015d76d9fcce1758f21e83e2464ee698c
Mehedi Hasan Himel <mehedihasanhimel89@gmail.com>
2024-09-04 10:10:00 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,19 @@
+/* const number = 7;
+
+if (number < 10) {
+  console.log(`Choto ekta number paici`);
+}
+
+console.log(`Something new is cooking.`);
+
+ */
+
+// While Loop
+
+let num = 0;
+while (num < 5) {
+  console.log(`looping...`, num);
+  num = num + 1;
+}
+
+ 
